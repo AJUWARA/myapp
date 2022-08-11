018dc96b0e6df28c35bec9da0b0648435820a96b
AJUWARA <70711510+AJUWARA@users.noreply.github.com>
2022-08-11 10:02:33 +0200

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[BOSS]'
+const name = '[Abubacar Juwara]'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
